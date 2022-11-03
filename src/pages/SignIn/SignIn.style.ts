8cc699f43f091cc8014c76b51b76c419e525c9fd
inhyuk15 <15inhyuk@gmail.com>
2022-11-03 21:35:34 +0900

    feat : login page 추가

--- src/pages/SignIn/SignIn.style.ts
+++ src/pages/SignIn/SignIn.style.ts
@@ -11,3 +11,45 @@ export const Container = styled(FlexCenter)`
     padding: 20px;
   }
 `;
+
+export const SignInBody = styled.div`
+  width: 50%;
+`;
+
+export const InputBox = styled.div`
+  margin-top: 0.75em;
+`;
+
+export const SignInLabel = styled.text`
+  grid-row: 1;
+  font-size: 0.75rem;
+  font-weight: 600;
+  color: #838383;
+`;
+
+export const SignInInput = styled.input`
+  border: 1px solid #afafaf;
+  border-radius: 5px;
+  padding: 0 65px 0 5px;
+  margin-top: 0.2rem;
+  width: 100%;
+  height: 35px;
+  outline: none;
+  ::placeholder {
+    color: #c3c3c3;
+    size: 0.75rem;
+  }
+`;
+
+export const LoginButton = styled.button`
+  top: 0;
+  right: 0;
+  border: none;
+  border-radius: 5px;
+  width: 100%;
+  height: 35px;
+  margin-top: 0.75rem;
+  background-color: #001e75;
+  color: #fff;
+  font-size: 0.75rem;
+`;
--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,17 +1,45 @@
 import { logo } from "@images/common";
-import { Link } from "react-router-dom";
-import { Title1 } from "~/constants/Typography";
+// import { Link } from "react-router-dom";
+import { useState } from "react";
 import * as Styled from "./SignIn.style";
 
 const SignIn = () => {
+  const [adminId, setAdminId] = useState("");
+  const [adminPw, setAdminPw] = useState("");
+
   return (
     <Styled.Container as="main" direction="column">
       <img src={logo} alt="logo" />
-
-      <Title1>츠누봇 관리자 페이지</Title1>
-      <Link to="/main" type="button">
-        메인 페이지가기
-      </Link>
+      <Styled.SignInBody>
+        <Styled.InputBox>
+          <Styled.SignInLabel> 아이디 </Styled.SignInLabel>
+          <Styled.SignInInput
+            type="text"
+            placeholder="아이디"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setAdminId(e.target.value);
+            }}
+          />
+        </Styled.InputBox>
+        <Styled.InputBox>
+          <Styled.SignInLabel> 비밀번호 </Styled.SignInLabel>
+          <Styled.SignInInput
+            type="text"
+            placeholder="비밀번호"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setAdminPw(e.target.value);
+            }}
+          />
+        </Styled.InputBox>
+        <Styled.LoginButton
+          onClick={() => {
+            // 구현 필요 서버에 보내서 아이디 체크
+            alert(adminId + adminPw);
+          }}
+        >
+          로그인
+        </Styled.LoginButton>
+      </Styled.SignInBody>
     </Styled.Container>
   );
 };
